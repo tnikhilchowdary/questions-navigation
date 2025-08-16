dc601f7ecc9211fab81610f1a600604cf5322458
THUMMALAPALLI NIKHIL CHOWDARY <tnikhilchowdary@gmail.com>
2025-08-16 07:57:58 -0500

    question array

--- questions-navigation/src/Questionsform.tsx
+++ questions-navigation/src/Questionsform.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 
+
+const Questions = [
+  {label:"What is your name? ", name:"name", type:"text"},
+  {label:"What is your age? ", name:"age", type:"text"},
+  {label:"Where do you live?", name:"location", type:"text"},
+];
+
 export default function Questionsform(){
+  const currentQuestion = Questions[0]
   return(
     <div>
-      <h1>Questions 1 of 3</h1>
-      <label>What is your name?</label>
+      <h1>Question 1 of {Questions.length}</h1>
+      <label>{currentQuestion.label}</label>
       <input type="text" className="w-full border p-2 rounded"/>
     </div>
   )
